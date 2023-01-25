2345f8659e16f090e949311524741c08b53044d6
rodrigonwl <rcbsa@protonmail.com>
2023-01-25 13:28:25 -0300

    Day19

---
+++ C#/HackerRank/Day19/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public interface AdvancedArithmetic
+{
+    int divisorSum(int n);
+}
+
+public class Calculator : AdvancedArithmetic
+{
+    public int divisorSum(int n)
+    {
+        int sum = 0;
+        for(int divisor = 1; divisor <= n; divisor++)
+        {
+            if (n % divisor == 0)
+                sum += divisor;
+        }
+
+        return sum;
+    }
+}
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Int32.Parse(Console.ReadLine());
+        AdvancedArithmetic myCalculator = new Calculator();
+        int sum = myCalculator.divisorSum(n);
+        Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum);
+    }
+}
